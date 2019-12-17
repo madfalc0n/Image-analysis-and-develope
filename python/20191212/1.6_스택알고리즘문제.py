4535018124b80d00e7c2a64fe336beb14a910291
Myoung Hwan Kim <chadool116@naver.com>
2019-12-17 18:32:04 +0900

    update python note 201912171832

--- python/20191212/1.6_스택알고리즘문제.py
+++ python/20191212/1.6_스택알고리즘문제.py
@@ -6,17 +6,27 @@ Created on Thu Dec 12 18:11:59 2019
 """
 
 #1번
-value = int(input("정수 K 입력(1 ≤ K ≤ 100,000)"))
-result_list = []
-for i in range(value):
-    input_value = int(input("{0}번째 정수 입력: " .format(i+1)))
-    if input_value != 0:
-        result_list.append(input_value)
-    elif input_value == 0:
-        result_list.pop()
-    
-print(result_list)
-print(sum(result_list))
+
+
+loop_v = True
+while loop_v:
+    value = int(input("정수 K 입력(1 ≤ K ≤ 100,000)"))
+    if 1<= value <= 100000:
+        loop_v = False
+        result_list = []
+        for i in range(value):
+            input_value = int(input("{0}번째 정수 입력: " .format(i+1)))
+            if input_value != 0:
+                result_list.append(input_value)
+            elif input_value == 0:
+                result_list.pop()
+            
+        print(result_list)
+        print(sum(result_list))
+    else:
+        print("다시 입력하시오.")
+
+
 
 
 
@@ -39,25 +49,29 @@ def detect_ps(ps_value):
                         ps_value.pop(i)
                         ps_value.pop(i)
                         break
+
+loop_v = True
+while loop_v:
+    value = int(input("2에서 50 사이 정수 입력"))
+    if 2<= value <= 50:
+        loop_v = False        
+        ps_list= []
+        for i in range(value): #괄호 입력받기
+            ps_value = str(input("{0}번쨰 값: " .format(i+1)))
+            ps_list.append(list(ps_value))
+        #print(ps_list)
         
-    
-    
-value = int(input("2에서 50 사이 정수 입력"))
-ps_list= []
-for i in range(value): #괄호 입력받기
-    ps_value = str(input("{0}번쨰 값: " .format(i+1)))
-    ps_list.append(list(ps_value))
-print(ps_list)
-
-result_list = []
-for j in ps_list:
-    if len(j)%2==0:#짝수면 판별해야됨
-        if j[0] == ')' or j[len(j)-1] == '(':
-            result_list.append("NO")
-        else:
-            result_list.append(detect_ps(j))
+        result_list = []
+        for j in ps_list:
+            if len(j)%2==0:#짝수면 판별해야됨
+                if j[0] == ')' or j[len(j)-1] == '(':
+                    result_list.append("NO")
+                else:
+                    result_list.append(detect_ps(j))
+            else:
+                result_list.append("NO")
+        
+        for x in result_list:#결과 출력
+            print(x)
     else:
-        result_list.append("NO")
-
-for x in result_list:#결과 출력
-    print(x)
+        print("다시 입력하시오..")
